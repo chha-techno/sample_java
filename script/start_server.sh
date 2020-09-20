11b691d54ccbba2e441a6dc08739da6722477de1
RajaniKatikala <RajaniKatikala@users.noreply.github.com>
2020-09-20 01:27:32 -0400

    Update start_server.sh

--- script/start_server.sh
+++ script/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /opt/tomcat
-export JAVA_HOME=$(dirname $dirname $(readlink -e /usr/bin/javac)));bin/bash /opt/tomcat/bin/startup.sh
+export JAVA_HOME=$(dirname $dirname $(readlink -e /usr/bin/javac)));/bin/bash /opt/tomcat/bin/startup.sh
